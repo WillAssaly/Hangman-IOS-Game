2baa4ae1db332eb0e3ba320848bca75db571468f
William Assaly <willassaly@gmail.com>
2023-08-13 11:26:25 -0400

    Hangman images added
    Hangman images added to ImagesSequenceData.

--- Hangman/Model/ImagesSequenceData.swift
+++ Hangman/Model/ImagesSequenceData.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+
+let imageNamesSequence = [
+"0_echafaud",
+"1_tete",
+"2_corps",
+"3_bras_gauche",
+"4_bras_droite",
+"5_jambe_gauche",
+"6_jambe_droite",
+"7_final"
+]
