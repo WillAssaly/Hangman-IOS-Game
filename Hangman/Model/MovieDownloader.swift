af3d1375e3ca90618e5273654b51295c8c5a6ef9
William Assaly <willassaly@gmail.com>
2023-11-09 02:12:06 -0500

    MovieDownloader Comments

--- Hangman/Model/MovieDownloader.swift
+++ Hangman/Model/MovieDownloader.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-
+// Represents a movie with its title, year, director, and IMDb ID.
 struct Movie: Decodable {
     let Title: String
     let Year: String
@@ -16,35 +16,38 @@ struct Movie: Decodable {
     
 }
 
+// Manages the downloading of movie details from an online database.
 class MovieDownloader {
     
 
+    static let shared = MovieDownloader()     // Singleton instance to be used throughout the app.
+    private let apiKey = "750448aa"         // The API key for accessing the movie database.
     
-    static let shared = MovieDownloader()
-    
-    
-    private let apiKey = "750448aa"
-    
+    // Fetches movie details from the OMDb API using a movie's IMDb ID.
     func fetchMovieDetails(by id: String, completion: @escaping (Movie?) -> Void) {
+        // Construct the URL for the API request using the given IMDb ID and the stored API key.
         guard let url = URL(string: "https://www.omdbapi.com/?i=\(id)&apikey=\(apiKey)") else {
-            completion(nil)
+            completion(nil)                     // Call completion handler with nil if URL is not valid.
             return
         }
         
+        // Create a data task to fetch the details from the URL.
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            // Check if data was received.
             if let data = data {
                 let decoder = JSONDecoder()
+                                           // Attempt to decode the data into a Movie object.
                 if let movie = try? decoder.decode(Movie.self, from: data) {
-                    completion(movie)
+                    completion(movie)    // Pass the movie object to the completion handler.
                 } else {
-                    completion(nil)
+                    completion(nil)    // Pass nil to the completion handler if decoding fails.
                 }
             } else {
-                completion(nil)
+                completion(nil)     // Pass nil to the completion handler if no data is received.
             }
         }
         
-        task.resume()
+        task.resume()           // Start the task to fetch the data.
     }
 }
 
