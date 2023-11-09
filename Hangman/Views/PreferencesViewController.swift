53cef1853620264fb1172c8b979bb44946140fcc
William Assaly <willassaly@gmail.com>
2023-11-09 01:08:51 -0500

    PreferencewViewController Comments

--- Hangman/Views/PreferencesViewController.swift
+++ Hangman/Views/PreferencesViewController.swift
@@ -7,6 +7,7 @@
 import Foundation
 import UIKit
 
+// ViewController to handle user preferences such as language and theme.
 class PreferencesViewController: UIViewController {
     
     @IBOutlet weak var languageSegmentedControl: UISegmentedControl!
@@ -14,20 +15,30 @@ class PreferencesViewController: UIViewController {
     
    
 
-
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
            super.viewDidLoad()
-           
+                                       // Load the previously saved user preferences.
            loadUserPreferences()
        }
-       
+                                                                              // Loads and applies the user's language and theme preferences.
        func loadUserPreferences() {
+           
            let selectedLanguage = UserDefaults.standard.string(forKey: "userLanguage") ?? "English"
+           
+                                                                          // Fetch the stored theme or default to "Dark".
+           
            let selectedThemeRawValue = UserDefaults.standard.string(forKey: "userTheme") ?? "Dark"
+           
+                                                                       // Convert the stored theme to a Theme enum or default to .device.
+           
            let selectedTheme = Theme(rawValue: selectedThemeRawValue) ?? .device
+           
+                                                                    // Set the segmented control to match the stored preferences.
            
            languageSegmentedControl.selectedSegmentIndex = selectedLanguage == "English" ? 0 : 1
            themeSegmentedControl.selectedSegmentIndex = {
+                                                                // Map the Theme enum to the segmented control index.
                switch selectedTheme {
                case .device:
                    return 0
@@ -38,17 +49,21 @@ class PreferencesViewController: UIViewController {
                }
            }()
        }
-       
+        // MARK: - Actions
+        // Called when the language selection is changed.
        @IBAction func languageChanged(_ sender: UISegmentedControl) {
+                                                                      // Determine the selected language based on the segmented control index.
            let selectedLanguage = sender.selectedSegmentIndex == 0 ? "English" : "Francais"
+           
+                                                                    // Save the selected language to UserDefaults.
            UserDefaults.standard.set(selectedLanguage, forKey: "userLanguage")
        }
     
     
-    
+    // Called when the theme selection is changed.
     @IBAction func themeChanged(_ sender: UISegmentedControl) {
         var selectedTheme: Theme
-        
+                                                             // Determine the selected theme based on the segmented control index.
         switch sender.selectedSegmentIndex {
                 case 0:
                     selectedTheme = .device
@@ -59,9 +74,10 @@ class PreferencesViewController: UIViewController {
                 default:
                     selectedTheme = .device
                 }
+                                                     // Save the selected theme to UserDefaults.
                 UserDefaults.standard.set(selectedTheme.rawValue, forKey: "userTheme")
                 
-                // Apply theme immediately
+                                                  // Apply theme immediately
                 if let sceneDelegate = view.window?.windowScene?.delegate as? SceneDelegate {
                     sceneDelegate.applyTheme(theme: selectedTheme)
                 }
